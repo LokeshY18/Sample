b67587042d3de2496b3424c43932413f68725456
Lokesh Yarashi <lyar0001@synchronoss.com>
2026-02-23 17:46:26 +0530

    Initial Commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by Lokesh Yarashi on 23/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
