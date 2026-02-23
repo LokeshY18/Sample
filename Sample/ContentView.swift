7ad99f26d857be6dc5fdf29f70c023c7fa872b79
Lokesh Yarashi <lyar0001@synchronoss.com>
2026-02-23 17:49:31 +0530

    Change System Icon

--- Sample/ContentView.swift
+++ Sample/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "gear")
+                .font(.largeTitle)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
